057fb64466c5a9e0267be0489d942cb855e74e1d
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-12-28 13:27:29 +0300

    SILVA SPARK MD
    120363200367779016@newsletter

--- plugins/prank.js
+++ plugins/prank.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             '',
-            '> *KHAN-AI-HACKING-COMPLETE ☣*'
+            '> *SILVA SPARK HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
