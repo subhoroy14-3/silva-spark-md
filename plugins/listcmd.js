149a73f9317cdbf17baed3ef11f59cced590198a
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-12-28 14:47:16 +0300

    120363200367779016@newsletter

--- plugins/listcmd.js
+++ plugins/listcmd.js
@@ -297,15 +297,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/89xq3r.jpg` },
+                image: { url: `https://i.imgur.com/PEZ5QL2.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363200367779016@newsletter',
+                        newsletterName: 'SILVA TECH INC🥰🦄',
                         serverMessageId: 143
                     }
                 }
