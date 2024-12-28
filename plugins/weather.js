30d34a9b9e036501d7b5b82fe0dc398329429152
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-12-28 16:18:47 +0300

    SILVA SPARK MD

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By JawadTechX*
+> *© Powdered By Silva Spark*
 `;
         return reply(weather);
     } catch (e) {
