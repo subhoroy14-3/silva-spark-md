903596863ae7e7a4f5348b050877cb25f2ee9659
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-12-28 15:18:01 +0300

    ⎈ Sɪʟᴠᴀ Ｓᴘᴀʀᴋ мᎠ ⎈

--- plugins/ownermsg.js
+++ plugins/ownermsg.js
@@ -134,7 +134,7 @@ async (conn, mek, m, { from, isOwner, quoted, reply }) => {
     const user = quoted.sender;
     try {
         await conn.updateBlockStatus(user, 'unblock');
-        reply(`✅ User ${user} unblocked successfully.`);
+        reply(`✅⎈ Sɪʟᴠᴀ Ｓᴘᴀʀᴋ мᎠ ⎈ User ${user} unblocked successfully.`);
     } catch (error) {
         reply(`❌ Error unblocking user: ${error.message}`);
     }
