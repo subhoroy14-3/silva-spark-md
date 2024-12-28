936d7aeb70c0a915e05772b7eb10e29483ca6691
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-12-28 15:14:09 +0300

    ⎈ Sɪʟᴠᴀ Ｓᴘᴀʀᴋ мᎠ ⎈

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX
+  ©⎈ Sɪʟᴠᴀ Ｓᴘᴀʀᴋ мᎠ ⎈
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
